a1519e0f3d3bfe2683953b9a6ed242ea323b93be
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-12-05 13:00:32 +0300

    working on navbar

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -1,7 +1,7 @@
 {{-- Navbar --}}
 <nav x-data="{mobileMenu: false}"
     class="lg:container fixed top-0 w-full flex flex-col justify-center items-center z-50 h-4 py-10">
-    <div class="w-full flex justify-between items-center px-10 lg:px-0">
+    <div class="w-full flex justify-between items-center px-2 md:px-10 lg:px-0">
         <div class="w-full">
             <img src="{{asset('img/logo/va_logo.svg')}}" alt="Victory Arch Logo">
         </div>
@@ -35,9 +35,9 @@
             </ul>
         </div>
 
-        <div class="w-full flex justify-end items-center gap-5 lg:gap-0">
+        <div class="w-full flex justify-end items-center gap-3 lg:gap-0">
             <a
-                class="btn bg-primary/90 hover:bg-primary focus:ring-1 focus:ring-secondary/50 rounded-lg px-4 py-2 border-0 text-white">
+                class="hidden lg:block bg-primary/90 hover:bg-primary focus:ring-1 focus:ring-secondary/50 rounded-lg px-4 py-2 border-0 text-white">
                 Start Project
             </a>
 
@@ -49,34 +49,42 @@
     </div>
 
     <div class="relative lg:hidden w-full flex justify-between items-center">
-        <div x-show="mobileMenu" class="absolute w-full right-0 left-0 top-3 bg-white px-10 lg:px-0 py-5"
-            x-data="{ mobileSolutionMenu: false }">
-            <ul class="flex flex-col justify-start items-end gap-4">
-                <li class="text-primary"><a href="#">HOME</a></li>
+        <template x-if="mobileMenu">
+            <div x-show="mobileMenu" class="absolute w-full right-0 left-0 top-3 bg-white px-10 lg:px-0 py-5"
+                x-data="{ mobileSolutionMenu: false }">
+                <ul class="flex flex-col justify-start items-end gap-4">
+                    <li class="text-primary"><a href="#">HOME</a></li>
 
-                <li class="text-primary"><a href="#">ABOUT</a></li>
+                    <li class="text-primary"><a href="#">ABOUT</a></li>
 
-                <li class="text-primary relative">
-                    <button @click="mobileSolutionMenu = !mobileSolutionMenu" @click.away="mobileSolutionMenu = false">
-                        SOLUTIONS
-                    </button>
+                    <li class="text-primary relative">
+                        <button @click="mobileSolutionMenu = !mobileSolutionMenu"
+                            @click.away="mobileSolutionMenu = false">
+                            SOLUTIONS
+                        </button>
 
-                    {{-- <ul x-show="mobileSolutionMenu" x-transition:enter="transition ease-out duration-200"
-                        x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100"
-                        x-transition:leave="transition ease-in duration-150"
-                        x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95"
-                        class="absolute w-[250px] bg-white shadow-md rounded-lg">
-                        <li class="w-full text-primary"><a href="#">Business Solutions</a></li>
-                        <li class="w-full text-primary"><a href="#">Hardware Products</a></li>
-                        <li class="w-full text-primary"><a href="#">Security</a></li>
-                        <li class="w-full text-primary"><a href="#">Infrastructure</a></li>
-                    </ul> --}}
-                </li>
+                        {{-- <ul x-show="mobileSolutionMenu" x-transition:enter="transition ease-out duration-200"
+                            x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100"
+                            x-transition:leave="transition ease-in duration-150"
+                            x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95"
+                            class="absolute w-[250px] bg-white shadow-md rounded-lg">
+                            <li class="w-full text-primary"><a href="#">Business Solutions</a></li>
+                            <li class="w-full text-primary"><a href="#">Hardware Products</a></li>
+                            <li class="w-full text-primary"><a href="#">Security</a></li>
+                            <li class="w-full text-primary"><a href="#">Infrastructure</a></li>
+                        </ul> --}}
+                    </li>
 
-                <li class="text-primary"><a href="#">SHOWROOM</a></li>
+                    <li class="text-primary"><a href="#">SHOWROOM</a></li>
 
-                <li class="text-primary"><a href="#">CONTACT</a></li>
-            </ul>
-        </div>
+                    <li class="text-primary"><a href="#">CONTACT</a></li>
+
+                    <a
+                        class="bg-primary/90 hover:bg-primary focus:ring-1 focus:ring-secondary/50 rounded-lg px-4 py-2 border-0 text-white">
+                        Start Project
+                    </a>
+                </ul>
+            </div>
+        </template>
     </div>
 </nav>
